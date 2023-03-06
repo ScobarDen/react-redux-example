4abd0585c79ea45d345632f2b2bcf890d389203d
ScobarDen <gdv-ps@yandex.ru>
2023-03-06 20:57:14 +0300

    redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
+function taskReducer(state, action) {
+  switch (action.type) {
+    case "task/completed": {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray[elementIndex].completed = true;
+      state = newArray;
+      console.log(state);
+      return state;
+    }
+    default:
+      break;
+  }
+}
+
+function createStore(initialState, reducer) {
+  let state = initialState;
+  const getState = () => state;
+  const dispatch = (action) => {
+    state = reducer(state, action);
+  };
+  return { getState, dispatch };
+}
+
 function App() {
-  return <div></div>;
+  const store = createStore(
+    [
+      { id: 1, description: "task1", completed: false },
+      { id: 2, description: "task2", completed: false },
+    ],
+    taskReducer
+  );
+
+  return (
+    <>
+      <div>{JSON.stringify(store.getState())}</div>
+      <button
+        onClick={() =>
+          store.dispatch({ type: "task/completed", payload: { id: 1 } })
+        }
+      >
+        Изменить
+      </button>
+    </>
+  );
 }
 
 export default App;
