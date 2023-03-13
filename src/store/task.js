7ff22144a72e826927a417153ab36dabfd4b76c3
ScobarDen <gdv-ps@mail.ru>
2023-03-13 16:12:06 +0300

    toolkit

--- src/store/task.js
+++ src/store/task.js
@@ -1,12 +1,26 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
   { id: 1, title: "task1", completed: false },
   { id: 2, title: "task2", completed: false },
 ];
 
-const update = createAction("task/updated");
-const remove = createAction("task/removed");
+const taskSlice = createSlice({
+  name: "task",
+  initialState,
+  reducers: {
+    update(state, action) {
+      const elementIndex = state.findIndex((el) => el.id === action.payload.id);
+      state[elementIndex] = { ...state[elementIndex], ...action.payload };
+    },
+    remove(state, action) {
+      return state.filter((el) => el.id !== action.payload.id);
+    },
+  },
+});
+
+const { actions, reducer: taskReducer } = taskSlice;
+const { update, remove } = actions;
 
 export function taskCompleted(id) {
   return update({ id, completed: true });
@@ -19,16 +33,5 @@ export const titleChanged = (id) => {
 export const taskDeleted = (id) => {
   return remove({ id });
 };
-
-const taskReducer = createReducer(initialState, (builder) => {
-  builder
-    .addCase(update, (state, action) => {
-      const elementIndex = state.findIndex((el) => el.id === action.payload.id);
-      state[elementIndex] = { ...state[elementIndex], ...action.payload };
-    })
-    .addCase(remove, (state, action) => {
-      return state.filter((el) => el.id !== action.payload.id);
-    });
-});
 
 export default taskReducer;
